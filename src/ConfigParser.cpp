14cdde6e50e3069d7a1669358689448917c549f6
Dafna Magid <dafna.magid@gmail.com>
2017-12-06 23:33:58 +0200

    a

---
+++ src/ConfigParser.cpp
@@ -0,0 +1,5 @@
+//
+// Created by dafnamagid on 12/6/17.
+//
+
+#include "ConfigParser.h"
---
+++ src/ConfigParser.h
@@ -0,0 +1,14 @@
+//
+// Created by dafnamagid on 12/6/17.
+//
+
+#ifndef TASK_04_ALL_CONFIGPARSER_H
+#define TASK_04_ALL_CONFIGPARSER_H
+
+
+class ConfigParser {
+
+};
+
+
+#endif //TASK_04_ALL_CONFIGPARSER_H
